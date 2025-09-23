cc249cbd9b4941ce3600cff33eea161e038f61a7
AnGgIt886 <jarbull86@gmail.com>
2025-09-23 18:17:25 +0700

    test

--- test.sh
+++ test.sh
@@ -3,8 +3,8 @@
 git config --global user.name "AnGgIt886"
 git config --global user.email "jarbull86@gmail.com"
 echo "DumpFirmware" >> .gitlab_group
-echo "${{ secrets.MY_TOKEN }}" >> .gitlab_token
-echo "${{ secrets.TG_TOKEN }}" >> .tg_token
-echo "${{ secrets.TG_CHAT }}" >> .tg_chat
+echo ${MY_TOKEN} >> .gitlab_token
+echo ${TG_TOKEN} >> .tg_token
+echo ${TG_CHAT} >> .tg_cha
 export PUSH_TO_GITLAB="true"
 bash dumper.sh https://bn.d.miui.com/V14.0.5.0.SJQMIXM/miui_LIMEGlobal_V14.0.5.0.SJQMIXM_057bc83463_12.0.zip
