36813251facbd0a22cf8479f015b2e724a26b02c
AnGgIt886 <jarbull86@gmail.com>
2025-09-23 21:51:14 +0700

    test fw fatsboot tgz

--- test.sh
+++ test.sh
@@ -7,4 +7,4 @@ echo $MY_TOKEN >> .gitlab_token
 echo $TG_TOKEN >> .tg_token
 echo $TG_CHAT >> .tg_chat
 export PUSH_TO_GITLAB="true"
-bash dumper.sh https://bn.d.miui.com/V14.0.5.0.SJQMIXM/miui_LIMEGlobal_V14.0.5.0.SJQMIXM_057bc83463_12.0.zip
+bash dumper.sh https://bn.d.miui.com/V14.0.5.0.SJQMIXM/lime_global_images_V14.0.5.0.SJQMIXM_20230619.0000.00_12.0_global_917a4eb0bb.tgz
