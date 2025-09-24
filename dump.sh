90f042e63ef2cd536f0aab47dee11c4051240951
AnGgIt886 <jarbull86@gmail.com>
2025-09-24 22:26:35 +0700

    Samsung A15 4G android(15)

--- dump.sh
+++ dump.sh
@@ -8,4 +8,4 @@ echo $TG_TOKEN >> .tg_token
 echo $TG_CHAT >> .tg_chat
 export PUSH_TO_GITLAB="true"
 export TERM=xterm
-bash dumper.sh https://bn.d.miui.com/V14.0.5.0.SJQMIXM/lime_global_images_V14.0.5.0.SJQMIXM_20230619.0000.00_12.0_global_917a4eb0bb.tgz
+bash dumper.sh https://drive.usercontent.google.com/download?id=1N9zifGGiLEGbp6730WeaMyqfrDqh_C9m&export=download&authuser=0
