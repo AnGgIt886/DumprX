666b09f7289fc4811e81304ddfbd6e3dfdf37f1d
AnGgIt86 <jarbull86@gmail.com>
2025-10-31 19:56:21 +0700

    ??

--- dump.sh
+++ dump.sh
@@ -8,4 +8,4 @@ echo $TG_TOKEN >> .tg_token
 echo $TG_CHAT >> .tg_chat
 export PUSH_TO_GITLAB="true"
 export TERM=xterm
-bash dumper.sh https://bn.d.miui.com/OS1.0.19.0.UMPIDXM/moonstone_id_global_images_OS1.0.19.0.UMPIDXM_20250930.0000.00_14.0_id_e66ff3f55c.tgz
+bash dumper.sh https://sourceforge.net/projects/port/files/hyper2/PORT_xiaomi.eu_CHIME_OS2.0.7.0.VNBCNXM_15.zip/download
