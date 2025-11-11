60fc1243f89a7bc74841c29a6e09abea50dee0c3
AnGgIt86 <jarbull86@gmail.com>
2025-11-11 09:43:09 +0700

    Xiaomi 15 Pro (haotian)

--- dump.sh
+++ dump.sh
@@ -8,4 +8,4 @@ echo $TG_TOKEN >> .tg_token
 echo $TG_CHAT >> .tg_chat
 export PUSH_TO_GITLAB="true"
 export TERM=xterm
-bash dumper.sh https://bkt-sgp-miui-ota-update-alisgp.oss-ap-southeast-1.aliyuncs.com/OS1.0.24.0.UNCCNXM/houji_images_OS1.0.24.0.UNCCNXM_20231119.0000.00_14.0_cn_6026623c0c.tgz
+bash dumper.sh https://bkt-sgp-miui-ota-update-alisgp.oss-ap-southeast-1.aliyuncs.com/OS3.0.3.0.WOBCNXM/haotian_images_OS3.0.3.0.WOBCNXM_20251012.0000.00_16.0_cn_2647cce655.tgz
