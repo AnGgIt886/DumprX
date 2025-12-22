8441ccc76ba13e7ba08f584c06708938a3321366
AnGgIt86 <jarbull86@gmail.com>
2025-12-23 06:21:33 +0700

    Palu Haotian

--- dump.sh
+++ dump.sh
@@ -8,4 +8,4 @@ echo $TG_TOKEN >> .tg_token
 echo $TG_CHAT >> .tg_chat
 export PUSH_TO_GITLAB="true"
 export TERM=xterm
-bash dumper.sh https://bkt-sgp-miui-ota-update-alisgp.oss-ap-southeast-1.aliyuncs.com/OS1.0.29.0.ULIMIXM/miui_TAOYAOGlobal_OS1.0.29.0.ULIMIXM_3178566a85_14.0.zip
+bash dumper.sh https://bkt-sgp-miui-ota-update-alisgp.oss-ap-southeast-1.aliyuncs.com/OS3.0.5.0.WOBCNXM/haotian_images_OS3.0.5.0.WOBCNXM_20251028.0000.00_16.0_cn_3f7fc327c6.tgz
