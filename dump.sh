a47b446365afb34a78964d7fa8ca5c49fd21570f
AnGgIt86 <jarbull86@gmail.com>
2026-03-11 10:55:27 +0700

    Xiaomi 17 Pro Max

--- dump.sh
+++ dump.sh
@@ -8,4 +8,4 @@ echo $TG_TOKEN >> .tg_token
 echo $TG_CHAT >> .tg_chat
 export PUSH_TO_GITLAB="true"
 export TERM=xterm
-bash dumper.sh https://bkt-sgp-miui-ota-update-alisgp.oss-ap-southeast-1.aliyuncs.com/OS1.0.12.0.ULMCNXM/miui_MUNCH_OS1.0.12.0.ULMCNXM_8603781dea_14.0.zip
+bash dumper.sh https://bkt-sgp-miui-ota-update-alisgp.oss-ap-southeast-1.aliyuncs.com/OS3.0.45.0.WPBCNXM/popsicle_images_OS3.0.45.0.WPBCNXM_20260203.0000.00_16.0_cn_6200f9beae.tgz
