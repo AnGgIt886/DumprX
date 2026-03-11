6cf1cce996ad9f21ead826ea4cc25806a3279ac1
AnGgIt86 <jarbull86@gmail.com>
2026-03-11 12:37:07 +0700

    Xiaomi 15 (dada)

--- dump.sh
+++ dump.sh
@@ -8,4 +8,5 @@ echo $TG_TOKEN >> .tg_token
 echo $TG_CHAT >> .tg_chat
 export PUSH_TO_GITLAB="true"
 export TERM=xterm
-bash dumper.sh "https://bkt-sgp-miui-ota-update-alisgp.oss-ap-southeast-1.aliyuncs.com/OS3.0.301.0.WPBCNXM/popsicle-ota_full-OS3.0.301.0.WPBCNXM-user-16.0-ddacbff4b4.zip"
+chmod +x dumper.sh
+./dumper.sh "https://bkt-sgp-miui-ota-update-alisgp.oss-ap-southeast-1.aliyuncs.com/OS3.0.7.0.WOCCNXM/dada_images_OS3.0.7.0.WOCCNXM_20260113.0000.00_16.0_cn_c2f80ad0f9.tgz"
