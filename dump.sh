21ea213c5bf62bc387886bb105e4d90fcb3dbcaa
AnGgIt86 <jarbull86@gmail.com>
2026-03-11 11:13:28 +0700

    Recovery rom

--- dump.sh
+++ dump.sh
@@ -8,4 +8,4 @@ echo $TG_TOKEN >> .tg_token
 echo $TG_CHAT >> .tg_chat
 export PUSH_TO_GITLAB="true"
 export TERM=xterm
-bash dumper.sh https://bkt-sgp-miui-ota-update-alisgp.oss-ap-southeast-1.aliyuncs.com/OS3.0.45.0.WPBCNXM/popsicle_images_OS3.0.45.0.WPBCNXM_20260203.0000.00_16.0_cn_6200f9beae.tgz
+bash dumper.sh "https://bkt-sgp-miui-ota-update-alisgp.oss-ap-southeast-1.aliyuncs.com/OS3.0.301.0.WPBCNXM/popsicle-ota_full-OS3.0.301.0.WPBCNXM-user-16.0-ddacbff4b4.zip"
